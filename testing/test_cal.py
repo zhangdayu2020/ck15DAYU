65fbe5c14897543893ec07693578c79535082bae
zhangdayu2020 <ayuayuayumazi@163.com>
2020-10-18 09:45:30 +0800

    calculation demo cases added

--- testing/test_cal.py
+++ testing/test_cal.py
@@ -11,28 +11,34 @@ class TestCalc:
     def teardown_class(self):
         print("calculate ended")
 
+    def setup(self):
+        print("Case started")
+        self.calc = Calculator()
+
+    def teardown(self):
+        print("Case ended")
     @pytest.mark.parametrize(
         'a,b,expect', [
-            [100, 100, 200], [1, 3, 4], [0.1, 0.1, 0.2]
-        ], ids=["case1", "case2", "case3"]
+            [100, 100, 200], [1, 3, 4], [0.1, 0.1, 0.2], [-1, 1, 0], [-2, 1, -1], [1, -0.99, 0.01]
+        ], ids=["case1", "case2", "case3", "case4", "case5", "case6"]
     )
     def test_add(self, a, b, expect):
         # calc = Calculator()
         print(a)
         print(b)
-        print(expect)
+        print(round(expect, 4))
         result = self.calc.add(a, b)
-        assert result == expect
+        assert round(result, 4) == round(expect, 4)
 
     @pytest.mark.parametrize(
         'c,d,expect1', [
-            [100, 100, 10000], [1, 0, 0], [0.1, 0.1, 0.01]
-        ], ids=["case11", "case21", "case31"]
+            [100, 100, 10000], [1, 0, 0], [0.1, 0.1, 0.01], [-1, -1, 1], [-1, 2, -2], [0, 100, 0]
+        ], ids=["case11", "case21", "case31", "case41", "case51", "case61", ]
     )
     def test_mul(self, c, d, expect1):
         # calc = Calculator()
         print(c)
         print(d)
-        print(round(expect1, 2))
+        print(round(expect1, 4))
         result = self.calc.mul(c, d)
-        assert round(result, 2) == expect1
+        assert round(result, 4) == round(expect1, 4)
